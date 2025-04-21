ef649a6067bccae0380e34e4948918b10a305951
maxlautenbach <mx@unity-mail.de>
2025-04-21 16:56:33 +0200

    Changes One Agent Prompt

--- approaches/full_sentence/One_Agent/prompts.py
+++ approaches/full_sentence/One_Agent/prompts.py
@@ -4,7 +4,7 @@ react_agent_promt = PromptTemplate.from_template("""
 You are a expert for closed information extraction from text into knowledge graph compatible triples in turtle format. For this task you will receive a text, where the triples should be extracted from. Please go through this problem step-by-step in a Reasoning & Acting Style. In addition to your own chain of thought you can use tools. Nevertheless you are asked to include the following action syntax in every of your outputs:
 
 <next>
-    <id>INSERT ID OF NEXT STEP HERE.</id>
+    <id>INSERT AGENT ID OF NEXT STEP HERE.</id>
     <instruction>INSERT AN INSTRUCTION HERE, IF NEEDED.</instruction>
 </next>
 
@@ -19,8 +19,8 @@ For your task you will have access to the following tools:
     - Description: The uri search tool takes a comma-seperated list of search terms and search modes. It will respond with the 3 most similar URIs according to the search term.
     - Input Requirements: Search Term 1[Search Mode 1], Search Term 2[Search Mode 2]...
     - Search Modes: 
-        - [LABEL]: Search via a label for an URI of an entity or predicate. This might often meet, how the entities are written down in the text.
-        - [DESCR]: Search via a description for an URI of an entity or predicate. You will have to make up a description of the entity or predicate you are searching for.
+        - [LABEL]: Search via a label for an URI of an entity or predicate. This might often meet, how the entities are written down in the text. (Recommended to use as first search mode)
+        - [DESCR]: Search via a description for an URI of an entity or predicate. The provided search term has to be a description of the entity or predicate you are searching for. (Recommended to search non-found entities and predicates)
     - Example Input: <instruction>Angela Merkel[LABEL], chancellor of Germany from 2005 to 2021[DESCR]</instruction>
     
 When you decide to end the processing, make sure you include the resulting triples in turtle format. Every URI should be in the form of <INSERT URI HERE> or should use an according prefix. If you use any turtle prefixes, ensure that you introduce them at the beginning of the turtle output. Ignore your implicit knowledge about public knowledge graphs (i.e. Namespaces for properties or URIs mapped to labels) and make sure, that you only use URIs, that were previously extracted by the uri_detection_agent. For example do not use the wikidata prefix wdt for properties, when there is no URI extracted with http://www.wikidata.org/prop/direct/, instead use the URIs extracted.
