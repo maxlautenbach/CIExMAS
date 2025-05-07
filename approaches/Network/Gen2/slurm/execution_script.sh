2f10d4f93ceb25f168e887f0c687a253d1c7c4b2
Max Lautenbach <max.lautenbach@yahoo.de>
2025-05-07 12:49:28 +0200

    Eval Gen2

--- approaches/Network/Gen2/slurm/execution_script.sh
+++ approaches/Network/Gen2/slurm/execution_script.sh
@@ -85,7 +85,7 @@ fi
 
 # === Run Agent System ===
 echo "Running agent system..."
-python3 ./approaches/Network/Gen2/slurm/agent_system.py test 50 'NTT-PE-SU'
+python3 ./approaches/Network/Gen2/slurm/agent_system.py test 50 'R/UST-D/NTT'
 
 # === Final Cleanup ===
 echo "Job completed. Cleaning up..."
