c076d32b67af9a838578d03f05dad58bdf4d57d5
Max Lautenbach <max.lautenbach@yahoo.de>
2025-05-27 15:16:28 +0200

    Eval One Agent on TLT

--- approaches/One_Agent/slurm/execution_script.sh
+++ approaches/One_Agent/slurm/execution_script.sh
@@ -85,7 +85,7 @@ fi
 
 # === Run Agent System ===
 echo "Running agent system..."
-python3 ./approaches/One_Agent/slurm/agent_system.py test_text 50 'R/PE-D/DMT-D/NTT'
+python3 ./approaches/One_Agent/slurm/agent_system.py test_text 50 'R/PE-TLT'
 
 # === Final Cleanup ===
 echo "Job completed. Cleaning up..."
